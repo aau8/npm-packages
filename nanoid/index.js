b6896c930259db7ed000893b1c8c16b4e236acb6
Artyom Ugryumov <artiom.ygryumov@gmail.com>
2024-06-01 23:10:11 +0300

    удалить каталог nanoid

--- nanoid/index.js
+++
@@ -1,5 +0,0 @@
-import { nanoid, urlAlphabet, customAlphabet } from 'nanoid'
-
-console.log(nanoid())
-console.log(urlAlphabet) // useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict
-console.log(customAlphabet('12345', 10)(5))
